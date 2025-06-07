2f6177f5325d563a0e0e90f7d5a8664e8a2b82f9
Shawon Ashraf <shawon.ashraf@proton.me>
2025-06-07 11:52:21 +0600

    updated for groq

--- settings.py
+++ settings.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     reddit_client_secret: SecretStr
     reddit_user_agent: str
     post_url: str = "https://www.reddit.com/r/AskReddit/comments/1kplv0m/whats_the_worst_city_youve_ever_visited/"
+    groq_api_key: SecretStr
     
     class Config:
         env_file = ".env"
